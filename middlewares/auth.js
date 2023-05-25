9c0f4b55e0390c111fcb3dc59cd17a23cacb9b7c
HammadAhsan341 <leaderhammad37@gmail.com>
2023-05-25 22:02:22 +0500

    Your commit message

---
+++ middlewares/auth.js
@@ -0,0 +1,53 @@
+const jwt = require("jsonwebtoken");
+const config = require("../config/config");
+
+const verifyToken = async (req, res, next) => {
+  const { authorization } = req.headers;
+
+  if (!authorization || !authorization.startsWith("Bearer ")) {
+    return res.status(400).json({ success: false, msg: "Invalid token format." });
+  }
+
+  const token = authorization.split(" ")[1];
+
+  if (!token) {
+    return res.status(400).json({ success: false, msg: "Token is required for authentication." });
+  }
+
+  try {
+    const decoded = jwt.verify(token, config.secret_jwt);
+    req.user_id = decoded._id; // Extract and store the user ID from the token payload
+    next();
+  } catch (error) {
+    return res.status(403).json({ success: false, msg: "Invalid token." });
+  }
+};
+
+const verifyAdmin = async (req, res, next) => {
+  const { authorization } = req.headers;
+
+  if (!authorization || !authorization.startsWith("Bearer ")) {
+    return res.status(400).json({ success: false, msg: "Invalid token format." });
+  }
+
+  const token = authorization.split(" ")[1];
+
+  if (!token) {
+    return res.status(400).json({ success: false, msg: "Token is required for authentication." });
+  }
+
+  try {
+    const decoded = jwt.verify(token, config.secret_jwt);
+    req.user = decoded; // Store the decoded token payload in req.user
+
+    if (req.user.isAdmin) {
+      next(); // Allow access if the user is an admin
+    } else {
+      return res.status(403).json({ success: false, msg: "Access denied. User is not an admin." });
+    }
+  } catch (error) {
+    return res.status(404).json({ success: false, msg: "Invalid token." });
+  }
+};
+
+module.exports = { verifyToken, verifyAdmin };
