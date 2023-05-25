fdda32c9bce85bdf4142660a39716145c4392562
HammadAhsan341 <leaderhammad37@gmail.com>
2023-05-25 22:13:14 +0500

    Your commit message

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ mongoose.connect("mongodb://localhost:27017/VotingSystem_DB")
 .then(() => console.log('Connected to MongoDB'))
 .catch(err => console.error('Could not connect to MongoDB', err));
 
+const cors = require("cors");
+
+app.use(cors());
 //middlewares
 app.use(express.json());
 app.use('/user',userRoute);
